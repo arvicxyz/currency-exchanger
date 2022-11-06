7c9120b40cf5e59e3e22677638f400028b99739c
Amir Fahd Hadji Usop <dudeamirfahd@gmail.com>
2022-11-06 19:52:07 +0800

    Clean up base project

--- android-kotlin/CurrencyExchanger/app/src/main/java/com/redhalfstudios/currencyexchanger/MainActivity.kt
+++ android-kotlin/CurrencyExchanger/app/src/main/java/com/redhalfstudios/currencyexchanger/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.redhalfstudios.currencyexchanger
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.redhalfstudios.currencyexchanger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -29,7 +28,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar
+                .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
